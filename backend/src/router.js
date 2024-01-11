d38810003ef735fdcf554a4d6b3d376706bd05f6
Calmes <christophe.calmes@laplateforme.io>
2024-01-11 15:06:34 +0100

    validator

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,12 @@
 const express = require("express");
+
 const router = express.Router();
-const sessionControllers = require("./controllers/sessionControllers")
+const sessionControllers = require("./controllers/sessionControllers");
 
 const itemControllers = require("./controllers/itemControllers");
 const userControllers = require("./controllers/usersControllers");
+
+const validatorUser = require("./validator/validatorUser");
 
 // Session
 router.post("/login", sessionControllers.logIn);
@@ -19,7 +22,7 @@ router.delete("/items/:id", itemControllers.destroy);
 router.get("/users", userControllers.browse);
 router.get("/users/:id", userControllers.read);
 router.put("/users/about_me/:id", userControllers.edit);
-router.post("/users", userControllers.add);
+router.post("/users", validatorUser, userControllers.add);
 router.delete("/users/:id", userControllers.destroy);
 
 module.exports = router;
---
+++ backend/src/validator/validatorFields.js
@@ -0,0 +1,25 @@
+const sizeFields = (data, targetSizeMax, targetSizeMin) => {
+  console.info(data.length);
+  if (data.length < targetSizeMax) {
+    if (data.length > targetSizeMin) {
+      return true;
+    }
+  }
+  return false;
+};
+const isEmailValid = (email) => {
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return regex.test(email);
+};
+const adultVerified = (date) => {
+  const years18 = 18 * 365 * 24 * 3600;
+  const birthdayDate = Date.parse(date);
+  const nowTimeStamp = Date.now();
+  const intervalleTime = nowTimeStamp - birthdayDate;
+  if (intervalleTime > years18) {
+    return true;
+  }
+  return false;
+};
+module.exports = { sizeFields, isEmailValid, adultVerified };
