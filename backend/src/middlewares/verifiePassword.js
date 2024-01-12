73058adcde662740396f10e471f1b7b42cdd0835
Calmes <christophe.calmes@laplateforme.io>
2024-01-12 16:43:53 +0100

    debug

--- backend/src/middlewares/verifiePassword.js
+++ backend/src/middlewares/verifiePassword.js
@@ -3,18 +3,16 @@ const models = require("../models");
 
 const verifyPassword = async (req, res, next) => {
   const { password } = req.body;
-  
   try {
     const [[dataDB]] = await models.user.findByEmail(req.body.email);
     const verifiedUser = await argon2.verify(dataDB.password, password);
     console.info(verifiedUser);
-    if(verifiedUser) {
+    if (verifiedUser) {
       next();
     } else {
-      res.status(401).send("It's very bad bad password ! Coquinou...");
+      res.status(401).send("It's very bad bad password !");
     }
-
-  }catch (err) {
+  } catch (err) {
     res.status(500).send("Auth fail !");
   }
 };
