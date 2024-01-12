05b1c530f8d3286f55410083378dd6a11f3cd1b5
Calmes <christophe.calmes@laplateforme.io>
2024-01-12 16:41:14 +0100

    Mtoan solution !

--- backend/src/middlewares/verifiePassword.js
+++ backend/src/middlewares/verifiePassword.js
@@ -1,24 +1,20 @@
 const argon2 = require('argon2');
 const models = require("../models");
 
-const verifyPassword = (req, res, next) => {
+const verifyPassword = async (req, res, next) => {
   const { password } = req.body;
+  
   try {
-    models.user.findByEmail(req.body.email).then(async ([rows]) => {
-      if (rows[0] === null) {
-        res.sendStatus(404);
-      } else {
-        const passwordHash = rows[0].password;
-        console.info(passwordHash);
-        const verifiedUser = await argon2.verify(passwordHash, password);
-        if (verifiedUser) {
-          next();
-        } else {
-          res.status(401).send("Auth fail !");
-        }
-      }
-    });
-  } catch (err) {
+    const [[dataDB]] = await models.user.findByEmail(req.body.email);
+    const verifiedUser = await argon2.verify(dataDB.password, password);
+    console.info(verifiedUser);
+    if(verifiedUser) {
+      next();
+    } else {
+      res.status(401).send("It's very bad bad password ! Coquinou...");
+    }
+
+  }catch (err) {
     res.status(500).send("Auth fail !");
   }
 };
