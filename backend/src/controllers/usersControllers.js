25c8c71e2a988cb2ebaf2f2af4a8605ca5924364
Calmes <christophe.calmes@laplateforme.io>
2024-01-22 16:49:00 +0100

    profile, en cours de construction

--- backend/src/controllers/usersControllers.js
+++ backend/src/controllers/usersControllers.js
@@ -14,7 +14,7 @@ const getAll = (req, res) => {
 
 const getById = (req, res) => {
   models.user
-    .find(req.params.id)
+    .findUser(req.params.id)
     .then(([rows]) => {
       if (rows[0] == null) {
         res.sendStatus(404);
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import styles from "./styles/profile.module.css";
+
+const user = {
+  id: 65,
+  email: "Bozo@jaimail.com",
+  username: "TheDead",
+  city: "Los_Angeles",
+  birthday_date: "1984-12-09T23:00:00.000Z",
+  gender: 1,
+  about_me: null,
+  creation_date: "2024-01-19T12:40:42.000Z",
+  liked: null,
+  valid: 1,
+  profil_picture: null,
+  role: "user",
+  level: 1,
+};
 
 function Profile() {
-  return <div>Profile</div>;
+  const dateObject = new Date(user.creation_date);
+  const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const formattedDate = new Intl.DateTimeFormat("fr-FR", options).format(
+    dateObject
+  );
+
+  return (
+    <div>
+      <section>
+        <article>
+          <h1>Profile</h1>
+          <p>
+            <img href={user.profil_picture} alt={user.username} />
+          </p>
+          <p>
+            <ul>
+              <li> {user.username} </li>
+              <li>Registred since {formattedDate} </li>
+              <li>Role {user.role}</li>
+            </ul>
+          </p>
+        </article>
+      </section>
+      <section>
+        <article>
+          <h1>Description</h1>
+          <p>{user.about_me}</p>
+        </article>
+      </section>
+    </div>
+  );
 }
 
 export default Profile;
