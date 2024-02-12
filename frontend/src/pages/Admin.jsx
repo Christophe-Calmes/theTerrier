eb84c98a8f549d78ab1ea21b91bdc07261216d96
Calmes <christophe.calmes@laplateforme.io>
2024-02-12 14:33:44 +0100

    Solution de Adrien le sauveur

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAuthContext } from "../context/AuthProvider";
+import styles from "./styles/profile.module.css";
+import { getUserData } from "../services/utilities";
 
 function Admin() {
-  return <div>Admin</div>;
+  const { currentUser } = useAuthContext();
+
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    const isAdmin = async () => {
+      if (currentUser) {
+        console.log(`id${currentUser.id}`);
+        const dataUser = await getUserData(currentUser.id);
+        console.log(`result${dataUser}`);
+        setAdmin(dataUser);
+      }
+    };
+
+    isAdmin();
+  }, [currentUser]);
+
+  console.log(`Ligne 31 ${JSON.stringify(admin)}`);
+  return (
+    <div>
+      {admin?.role === "admin" ? (
+        <section>
+          <article>
+            <h1 className={styles.title1}>Administrateur</h1>
+          </article>
+        </section>
+      ) : (
+        <section>
+          <article>
+            <h1 className={styles.title1}>Pas administrateur</h1>
+          </article>
+        </section>
+      )}
+    </div>
+  );
 }
 
 export default Admin;
